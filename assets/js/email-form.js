8a939f4e37674ae8665b8ffe06a89545ef06509f
Johanna Erlandsson <johanna.e@gmail.com>
2020-12-27 14:17:25 +0100

    required fields in form

--- assets/js/email-form.js
+++ assets/js/email-form.js
@@ -3,4 +3,16 @@
  * Author: Johanna Erlandsson
  */
 
- 
+function validateForm() {
+    var x = document.forms["email-form"]["name"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+
+  /*<form name="myForm" action="/action_page.php" onsubmit="return validateForm()" 
+  method="post">
+  Name: <input type="text" name="fname">
+  <input type="submit" value="Submit">
+</form>*/
